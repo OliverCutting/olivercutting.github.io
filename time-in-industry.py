4a84148bd699568e7167562e3f5da961b65ed5ad
Oliver Cutting <oliver.cutting@engineering.digital.dwp.gov.uk>
2022-11-11 16:52:02 +0000

    add work experience sections

--- time-in-industry.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-start_date = datetime.datetime(2022,1,4)
-end_date = datetime.datetime.now()
-
-time_in_industry = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
-
-print(time_in_industry)
